68aac005a198b59f0f0e80509708b3815ef16ee6
Daniel Sage <dsage@umich.edu>
2025-02-24 13:54:58 -0500

    refactor: move ranking into common (#46)

--- ranking/src/_CrawlerTest.cpp
+++ ranking/src/_CrawlerTest.cpp
@@ -1,12 +1,18 @@
-#include "CrawlerRanker.h"
-#include <queue>
-#include <iostream>
+#include "ranking/CrawlerRanker.h"
+
+#include <cstddef>
+#include <cstdint>
 #include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
 
 int main() {
     std::vector<std::string> realURLs;
 
-    std::ifstream file("_Links.txt");
+    std::ifstream file("ranking/src/_Links.txt");
     std::string line;
     while (std::getline(file, line)) {
         realURLs.push_back(line);
@@ -14,14 +20,14 @@ int main() {
 
     std::priority_queue<std::pair<uint32_t, std::string>> crawlerQueue;
     for (const std::string& url : realURLs) {
-        uint32_t ranking = mithril::crawler_ranker::GetUrlRank(url);
+        uint32_t ranking = mithril::ranking::GetUrlRank(url);
         crawlerQueue.push({ranking, url});
     }
 
     size_t i = 1;
     while (!crawlerQueue.empty()) {
-        const auto &p = crawlerQueue.top();
-        
+        const auto& p = crawlerQueue.top();
+
         std::cout << i << ". " << p.second << " - " << p.first << std::endl;
         crawlerQueue.pop();
 
