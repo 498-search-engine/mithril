3b204e74e246db697081c135bed244ac0f650e83
Anubhav Agarwal <anubhava@umich.edu>
2025-03-17 20:30:38 -0400

    add dynamic tolerance

--- ranking/src/pagerank.cpp
+++ ranking/src/pagerank.cpp
@@ -67,7 +67,9 @@ int main(int argc, char *argv[]) {
     }
     srand(498);
 
-    std::cout << "simulating " << nodes << " nodes" << std::endl;
+    double tol = 1.0 / nodes;
+
+    std::cout << "simulating " << nodes << " nodes with precision of " << tol << std::endl;
 
     auto start = std::chrono::steady_clock::now();
 
@@ -114,7 +116,7 @@ int main(int argc, char *argv[]) {
     std::cout << "finished building graph in: " << duration.count() << " ms" << std::endl;
 
     start = std::chrono::steady_clock::now();
-    vector<double> result = PageRank(m, nodes);
+    vector<double> result = PageRank(m, nodes, tol=tol);
     end = std::chrono::steady_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "finished pagerank in: " << duration.count() << " ms" << std::endl;
@@ -126,3 +128,4 @@ int main(int argc, char *argv[]) {
     // cout << endl;
     return 0;
 }
+
