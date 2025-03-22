3950dfc891f5c273089b9da3b574fe12746100e5
iayasin771@gmail.com <iayasin771@gmail.com>
2025-03-22 15:33:32 -0400

    Further define the query language

--- query/src/Tokenizer.h
+++ query/src/Tokenizer.h
@@ -13,14 +13,11 @@
 
 // #include "expression.h"
 
-struct Expression {
 
-};
+// TODO: Change this
+struct Expression {};
 
-struct SimpleTerm : public Expression {
-
-
-};
+struct SimpleTerm : public Expression {};
 
 /**
  * Check if a character is relevant to a math expression
@@ -34,12 +31,16 @@ struct SimpleTerm : public Expression {
  *
  * and all digits
  */
-bool CharIsRelevant( char c );
+bool CharIsRelevant(char c){
+    
+}
 
 /**
- * Opposite of char is relevant. Needed for filtering the input.
- */
-bool CharIsIrrelevant( char c );
+    * Opposite of char is relevant. Needed for filtering the input.
+    */
+bool CharIsIrrelevant(char c){
+    
+}
 
 /**
  * The token stream, which you can both Match( ) a single character from,
@@ -48,39 +49,37 @@ bool CharIsIrrelevant( char c );
  * The input string by default is filtered of any characters that are
  * deemed "irrelevant" by the CharIsIrrelevant function above.
  */
-class TokenStream
-   {
-   // The input we receive, with only relevant characters left
-   std::string input;
-   // Where we currently are in the input
-   size_t location = 0;
+class TokenStream {
+    // The input we receive, with only relevant characters left
+    std::string input;
+    // Where we currently are in the input
+    size_t location = 0;
 
 public:
+    /**
+     * Construct a token stream that uses a copy of the input
+     * that contains only characters relevant to math expressions
+     */
+    TokenStream(const std::string& in);
 
-   /**
-    * Construct a token stream that uses a copy of the input
-    * that contains only characters relevant to math expressions
-    */
-   TokenStream( const std::string &in );
+    /**
+     * Attempt to match and consume a specific character
+     *
+     * Returns true if the char was matched and consumed, false otherwise
+     */
+    bool Match(char c);
 
-   /**
-    * Attempt to match and consume a specific character
-    *
-    * Returns true if the char was matched and consumed, false otherwise
-    */
-   bool Match( char c );
+    /**
+     * Check whether all the input was consumed
+     */
+    bool AllConsumed() const;
 
-   /**
-    * Check whether all the input was consumed
-    */
-   bool AllConsumed( ) const;
-
-   /**
-    * Attempt to match and consume a whole number
-    *
-    * Return a dynamically allocated Number if successful, nullptr otherwise
-    */
-   SimpleTerm *ParseSimpleTerm( );
-   };
+    /**
+     * Attempt to match and consume a whole number
+     *
+     * Return a dynamically allocated Number if successful, nullptr otherwise
+     */
+    SimpleTerm* ParseSimpleTerm();
+};
 
 #endif /* TOKENSTREAM_H_ */
