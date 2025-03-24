e59ee324519a9979e1350b96baac1dc00ad3dfc3
Daniel Sage <dsage@umich.edu>
2025-03-24 00:09:05 -0400

    replace usages of perror

--- common/src/Clock.cpp
+++ common/src/Clock.cpp
@@ -1,14 +1,17 @@
 #include "Clock.h"
 
-#include <cstdio>
+#include "spdlog/spdlog.h"
+
+#include <cerrno>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 
 long MonotonicTime() {
     struct timespec tp {};
     int status = clock_gettime(CLOCK_MONOTONIC, &tp);
     if (status < 0) [[unlikely]] {
-        perror("clock_gettime");
+        spdlog::critical("clock_gettime failed: {}", strerror(errno));
         exit(1);
     }
     return tp.tv_sec;
@@ -18,7 +21,7 @@ long MonotonicTimeMs() {
     struct timespec tp {};
     int status = clock_gettime(CLOCK_MONOTONIC, &tp);
     if (status < 0) [[unlikely]] {
-        perror("clock_gettime");
+        spdlog::critical("clock_gettime failed: {}", strerror(errno));
         exit(1);
     }
     return (tp.tv_sec * 1000) + (tp.tv_nsec / 1000000);
