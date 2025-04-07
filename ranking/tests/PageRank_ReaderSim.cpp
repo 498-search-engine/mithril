1cd96ff51b34b39d024ebdd80751684eb9ffc847
Anubhav Agarwal <anubhava@umich.edu>
2025-04-07 14:18:52 -0400

    more robust protection

--- ranking/tests/PageRank_ReaderSim.cpp
+++ ranking/tests/PageRank_ReaderSim.cpp
@@ -8,13 +8,16 @@ int main() {
     mithril::pagerank::PageRankReader reader;
     spdlog::info("Read data. Max docid: {}", reader.size_);
 
+    std::string docid_str;
     mithril::data::docid_t docid;
     
     while (true) {
         std::cout << "Enter docid: ";
-        std::cin >> docid;
-
-        if (docid >= reader.size_) {
+        std::cin >> docid_str;
+        
+        try {
+            docid = atoi(docid_str.c_str());
+        } catch (const std::exception& e) {
             std::cout << "Invalid docid. Exiting..." << std::endl;
             break;
         }
