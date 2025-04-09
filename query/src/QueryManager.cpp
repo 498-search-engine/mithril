98e7f93e2ad7375ebb33cb49f5e1cfc93fced0db
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-04-09 13:36:31 -0400

    first commit of query manager

---
+++ query/src/QueryManager.cpp
@@ -0,0 +1,5 @@
+#include "QueryManager.h"
+
+namespace mithril {
+
+}  // namespace mithril
---
+++ query/src/QueryManager.h
@@ -0,0 +1,50 @@
+#ifndef QUERY_QUERYMANAGER_H
+#define QUERY_QUERYMANAGER_H
+
+#include "QueryEngine.h"
+
+#include <vector>
+#include <string>
+#include <thread>
+#include <memory>
+#include <cstdint>
+#include <mutex>
+#include <condition_variable>
+
+namespace mithril {
+
+class QueryManager {
+public:
+    using QueryResult = std::vector<uint32_t>;
+
+    QueryManager(const std::vector<std::string>& index_dirs);
+
+    QueryManager(const QueryManager&) = delete;
+    QueryManager& operator=(const QueryManager&) = delete;
+
+    ~QueryManager();
+
+    QueryResult AnswerQuery(const std::string& query);
+
+private:
+    void WorkerThread(size_t worker_id);
+    
+
+private:
+    std::vector<std::thread> threads_;
+    std::vector<std::unique_ptr<QueryEngine>> query_engines_;
+    std::vector<QueryResult> maginal_results_;
+
+    std::mutex mtx_;
+    std::condition_variable main_cv_;
+    std::condition_variable worker_cv_;
+
+    bool stop_;
+    bool query_available_;
+    std::string current_query_;
+    size_t worker_completion_count_;
+}
+
+}  // namespace mithril
+
+#endif
