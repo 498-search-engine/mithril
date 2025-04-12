646b034f420dadbdc23766907645a3ec965f825d
ATShah03 <sarnav@umich.edu>
2025-04-12 09:44:46 -0400

    initial serialization schema done

---
+++ rpc-infra/BasicClient.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "rpc_handler.h"  
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+    if (connect(sockfd, (sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    std::vector<std::pair<uint32_t, std::string>> data = {
+        {1, "hello"},
+        {2, "world"},
+        {42, "rpc test"}
+    };
+
+    RPCHandler::Send(sockfd, data);
+    std::cout << "Data sent!\n";
+
+    close(sockfd);
+    return 0;
+}
