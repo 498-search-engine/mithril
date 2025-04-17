d8c2b2e3c4ec2defde2fbc8d186320a8b1429680
Anubhav Agarwal <anubhava@umich.edu>
2025-04-17 16:56:10 -0400

    fix

--- ranking/src/Ranker.h
+++ ranking/src/Ranker.h
@@ -8,7 +8,6 @@ namespace mithril::ranking {
  * e.g A and (B or A) => {“A”: 2”, “B”: 1}
  */
 uint32_t GetFinalScore(const std::vector<std::pair<std::string, int>>& query,
-                       ,
                        const data::Document& doc,
                        const data::DocInfo& info);
 }  // namespace mithril::ranking
